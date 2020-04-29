add6157f49cf1ce18257357134629fdc9470658f
Jana Laney <Jana.Laney@gmail.com>
2020-04-28 19:19:53 -0600

    Updates

---
+++ precache-manifest.e1006d61a2643afaea0eb9b2d03b0931.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e4f0a2469406d47c74ce91a0d2b92d",
+    "url": "/userdirectory/index.html"
+  },
+  {
+    "revision": "a9d7578979c6f3f3aa39",
+    "url": "/userdirectory/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "1e162969d02d88ec1206",
+    "url": "/userdirectory/static/css/main.53a1b234.chunk.css"
+  },
+  {
+    "revision": "a9d7578979c6f3f3aa39",
+    "url": "/userdirectory/static/js/2.8d8bda7b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/userdirectory/static/js/2.8d8bda7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e162969d02d88ec1206",
+    "url": "/userdirectory/static/js/main.d5292d7b.chunk.js"
+  },
+  {
+    "revision": "a6c5260629a9d946981e",
+    "url": "/userdirectory/static/js/runtime-main.ded7ef7f.js"
+  }
+]);
